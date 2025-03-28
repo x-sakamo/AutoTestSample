c47ea21e6540227bffd0722f67daa71f097b1f08
OEC\x_sakamo <x_sakamo@oec.co.jp>
2025-03-28 15:27:08 +0900

    テストコミット

---
+++ MyConsoleApp/MyConsoleApp/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp
+{
+    public class Calculator
+    {
+        public int Add(int input1, int input2)
+        {
+            return input1 + input2;
+        }
+    }
+}
---
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator myCalculator = new Calculator();
+            int result = myCalculator.Add(1,2);
+            Console.WriteLine(result);
+        }
+    }
+}
